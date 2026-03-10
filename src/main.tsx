760eb62ac833e476e794c124ed911675004773e7
Kaisa Leinonen <laurakaisaleinonen@gmail.com>
2026-03-10 14:33:53 +0200

    add empty theme and cssbaseline

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client'
-import { Container } from '@mui/material'
+import { Container, ThemeProvider, CssBaseline } from '@mui/material'
 import App from './App'
+import { theme } from './theme'
 
 const container = document.getElementById('root')
 
@@ -8,8 +9,13 @@ if (!container) {
 	throw new Error('Root container missing in index.html')
 }
 
+
+
 createRoot(container).render(
-	<Container>
-		<App />
-	</Container>
+	<ThemeProvider theme={theme}>
+		<CssBaseline />
+		<Container maxWidth="md">
+			<App />
+		</Container>
+	</ThemeProvider>
 )
